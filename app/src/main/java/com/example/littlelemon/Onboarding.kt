a77623aeaa4b297be7c55d88684a2219b9dc85c3
Brandon Wyie <brandon.wylie@una.ca>
2024-09-25 07:55:50 -0600

    navigation wip

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,35 +1,35 @@
 package com.example.littlelemon
 
-import android.net.http.HeaderBlock
-import android.os.Bundle
-import android.preference.PreferenceActivity.Header
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import android.annotation.SuppressLint
+import android.telecom.Call.Details
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -38,46 +38,69 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.littlelemon.ui.theme.LittleLemonTheme
+import androidx.navigation.NavHostController
 
 val karlaFontFamily = FontFamily(
     Font(R.font.karla_regular, FontWeight.Normal)
 )
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Onboarding(image: Painter, description: String, modifier: Modifier = Modifier) {
-    Column(
-        Modifier.fillMaxHeight(),
-        verticalArrangement = Arrangement.SpaceBetween
+fun Onboarding(navController: NavHostController) {
+    Scaffold(
+        bottomBar = {
+            Button(
+                onClick =
+                {
+                    if (validateFields()) {
+                        navController.navigate(Home.route)
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primary_yellow)),
+                shape = RoundedCornerShape(5.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.onboarding_register),
+                    fontSize = 20.sp,
+                    lineHeight = 20.sp,
+                    color = colorResource(id = R.color.highlight_black),
+                    fontFamily = karlaFontFamily,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     ) {
         Column(
-            verticalArrangement = Arrangement.Top,
-            modifier = modifier
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
                 .verticalScroll(rememberScrollState())
-                .weight(0.9f, true)
+                .fillMaxHeight()
+                .padding(0.dp, 0.dp, 0.dp, 80.dp)
         ) {
             Row(
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxWidth()
                     .height(100.dp)
             ) {
                 Image(
-                    painter = image,
-                    contentDescription = description,
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Little Lemon Logo",
                     contentScale = ContentScale.FillWidth,
-                    modifier = modifier
+                    modifier = Modifier
                         .fillMaxWidth(0.5f)
                 )
             }
             Row(
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxWidth()
                     .background(colorResource(id = R.color.primary_green))
                     .height(120.dp)
@@ -93,7 +116,7 @@ fun Onboarding(image: Painter, description: String, modifier: Modifier = Modifie
             }
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxWidth()
                     .padding(20.dp)
                     .height(80.dp)
@@ -107,40 +130,60 @@ fun Onboarding(image: Painter, description: String, modifier: Modifier = Modifie
                     fontWeight = FontWeight.Bold
                 )
             }
-            FormLine(label = stringResource(id = R.string.onboarding_firstname))
-            FormLine(label = stringResource(id = R.string.onboarding_lastname))
-            FormLine(label = stringResource(id = R.string.onboarding_email))
-        }
-        Column(
-            verticalArrangement = Arrangement.Center,
-            modifier = modifier
-                .weight(0.1f, true)
-        ) {
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primary_yellow)),
-                shape = RoundedCornerShape(5.dp),
-                modifier = modifier
-                    .weight(0.1f, false)
+            FormLabel(stringResource(id = R.string.onboarding_firstname))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
                     .fillMaxWidth()
-                    .padding(20.dp)
+                    .padding(20.dp, 10.dp)
             ) {
-                Text(
-                    text = stringResource(id = R.string.onboarding_register),
-                    fontSize = 20.sp,
-                    lineHeight = 20.sp,
-                    color = colorResource(id = R.color.highlight_black),
-                    fontFamily = karlaFontFamily,
-                    fontWeight = FontWeight.Bold
+                val firstName = OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
                 )
-
+            }
+            FormLabel(stringResource(id = R.string.onboarding_lastname))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 10.dp)
+            ) {
+                val lastName = OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                )
+            }
+            FormLabel(stringResource(id = R.string.onboarding_email))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 10.dp)
+            ) {
+                val email = OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        shape = RoundedCornerShape(5.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(40.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun FormLine(label: String, modifier: Modifier = Modifier) {
+fun FormLabel(label: String, modifier: Modifier = Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
@@ -157,26 +200,9 @@ fun FormLine(label: String, modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
     }
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(20.dp, 10.dp)
-    ) {
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            shape = RoundedCornerShape(5.dp),
-            modifier = modifier
-                .fillMaxWidth()
-                .height(40.dp))
-    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun OnboardingPreview() {
-    LittleLemonTheme {
-        Onboarding(painterResource(id = R.drawable.logo), "Little Lemon Logo")
-    }
+private fun validateFields(): Boolean {
+
+    return false
 }
