d637eac50265856c012a6adedf8c8d299883d3a7
Brandon Wyie <brandon.wylie@una.ca>
2024-09-25 07:56:57 -0600

    navigation wip

---
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -0,0 +1,25 @@
+package com.example.littlelemon
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(navController: NavHostController, startDestination: String) {
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(Onboarding.route) {
+            Onboarding(navController)
+        }
+        composable(Home.route) {
+            Home()
+        }
+        composable(Profile.route) {
+            Profile()
+        }
+    }
+}
+
