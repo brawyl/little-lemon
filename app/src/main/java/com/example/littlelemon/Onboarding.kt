03e9b15547ebd309fec703f4923b62c37c251066
Brandon Wyie <brandon.wylie@una.ca>
2024-09-26 16:24:03 -0600

    Added field validation for onboarding screen.

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,6 +1,7 @@
 package com.example.littlelemon
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.telecom.Call.Details
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
@@ -26,8 +29,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -37,6 +43,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,39 +56,18 @@ val karlaFontFamily = FontFamily(
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Onboarding(navController: NavHostController) {
-    Scaffold(
-        bottomBar = {
-            Button(
-                onClick =
-                {
-                    if (validateFields()) {
-                        navController.navigate(Home.route)
-                    }
-                },
-                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primary_yellow)),
-                shape = RoundedCornerShape(5.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
-            ) {
-                Text(
-                    text = stringResource(id = R.string.onboarding_register),
-                    fontSize = 20.sp,
-                    lineHeight = 20.sp,
-                    color = colorResource(id = R.color.highlight_black),
-                    fontFamily = karlaFontFamily,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-        }
+    var isValid by remember { mutableStateOf(false) }
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxHeight()
+            .padding(0.dp, 0.dp, 0.dp, 80.dp)
     ) {
-        Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .fillMaxHeight()
-                .padding(0.dp, 0.dp, 0.dp, 80.dp)
-        ) {
+        Column {
             Row(
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically,
@@ -119,7 +105,7 @@ fun Onboarding(navController: NavHostController) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(20.dp)
-                    .height(80.dp)
+                    .height(60.dp)
             ) {
                 Text(
                     text = stringResource(id = R.string.onboarding_information),
@@ -137,13 +123,18 @@ fun Onboarding(navController: NavHostController) {
                     .fillMaxWidth()
                     .padding(20.dp, 10.dp)
             ) {
-                val firstName = OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                OutlinedTextField(
+                    value = firstName,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    onValueChange = { input ->
+                        firstName = input
+                        isValid = input.isNotEmpty()
+                    },
+                    placeholder = { Text(text = stringResource(id = R.string.onboarding_firstname_placeholder)) },
+                    isError = firstName.isNotEmpty(),
                     shape = RoundedCornerShape(5.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(40.dp)
                 )
             }
             FormLabel(stringResource(id = R.string.onboarding_lastname))
@@ -153,13 +144,18 @@ fun Onboarding(navController: NavHostController) {
                     .fillMaxWidth()
                     .padding(20.dp, 10.dp)
             ) {
-                val lastName = OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                OutlinedTextField(
+                    value = lastName,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    onValueChange = { input ->
+                        lastName = input
+                        isValid = input.isNotEmpty()
+                    },
+                    placeholder = { Text(text = stringResource(id = R.string.onboarding_lastname_placeholder)) },
+                    isError = lastName.isNotEmpty(),
                     shape = RoundedCornerShape(5.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(40.dp)
                 )
             }
             FormLabel(stringResource(id = R.string.onboarding_email))
@@ -169,14 +165,48 @@ fun Onboarding(navController: NavHostController) {
                     .fillMaxWidth()
                     .padding(20.dp, 10.dp)
             ) {
-                val email = OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
-                        shape = RoundedCornerShape(5.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(40.dp)
+                OutlinedTextField(
+                    value = email,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    onValueChange = { input ->
+                        email = input
+                        isValid = input.isNotEmpty()
+                    },
+                    placeholder = { Text(text = stringResource(id = R.string.onboarding_email_placeholder)) },
+                    isError = lastName.isNotEmpty(),
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
                 )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Button(
+                    onClick =
+                    {
+                        if (isValid) {
+                            navController.navigate(Home.route)
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primary_yellow)),
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.onboarding_register),
+                        fontSize = 20.sp,
+                        lineHeight = 20.sp,
+                        color = colorResource(id = R.color.highlight_black),
+                        fontFamily = karlaFontFamily,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
     }
@@ -200,9 +230,4 @@ fun FormLabel(label: String, modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
     }
-}
-
-private fun validateFields(): Boolean {
-
-    return false
 }
