bd61119d555ddf4eb9cf3f16a49fe3da9794cd4e
Brandon Wyie <brandon.wylie@una.ca>
2024-09-27 13:27:25 -0600

    Added warning message when trying to register with blank fields.

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -20,18 +20,24 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -48,6 +54,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.launch
 
 val karlaFontFamily = FontFamily(
     Font(R.font.karla_regular, FontWeight.Normal)
@@ -60,151 +67,163 @@ fun Onboarding(navController: NavHostController) {
     var firstName by remember { mutableStateOf("") }
     var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
-    Column(
-        verticalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .fillMaxHeight()
-            .padding(0.dp, 0.dp, 0.dp, 80.dp)
-    ) {
-        Column {
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            Button(
+                onClick =
+                {
+                    if (isValid) {
+                        navController.navigate(Home.route)
+                    } else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("You must fill out all fields to continue.")
+                        }
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primary_yellow)),
+                shape = RoundedCornerShape(5.dp),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(100.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "Little Lemon Logo",
-                    contentScale = ContentScale.FillWidth,
-                    modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                )
-            }
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(colorResource(id = R.color.primary_green))
-                    .height(120.dp)
+                    .padding(20.dp, 0.dp)
             ) {
                 Text(
-                    text = stringResource(id = R.string.onboarding_prompt),
-                    fontSize = 25.sp,
-                    lineHeight = 25.sp,
-                    textAlign = TextAlign.Center,
-                    color = colorResource(id = R.color.highlight_white),
-                    fontFamily = karlaFontFamily
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .height(60.dp)
-            ) {
-                Text(
-                    text = stringResource(id = R.string.onboarding_information),
-                    fontSize = 20.sp,
-                    lineHeight = 20.sp,
+                    text = stringResource(id = R.string.onboarding_register),
+                    fontSize = 24.sp,
+                    lineHeight = 24.sp,
                     color = colorResource(id = R.color.highlight_black),
                     fontFamily = karlaFontFamily,
                     fontWeight = FontWeight.Bold
                 )
             }
-            FormLabel(stringResource(id = R.string.onboarding_firstname))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp, 10.dp)
-            ) {
-                OutlinedTextField(
-                    value = firstName,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                    onValueChange = { input ->
-                        firstName = input
-                        isValid = input.isNotEmpty()
-                    },
-                    placeholder = { Text(text = stringResource(id = R.string.onboarding_firstname_placeholder)) },
-                    isError = firstName.isNotEmpty(),
-                    shape = RoundedCornerShape(5.dp),
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    ) { contentPadding ->
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxHeight()
+                .padding(0.dp, 0.dp, 0.dp, 80.dp)
+        ) {
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
-                )
-            }
-            FormLabel(stringResource(id = R.string.onboarding_lastname))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp, 10.dp)
-            ) {
-                OutlinedTextField(
-                    value = lastName,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                    onValueChange = { input ->
-                        lastName = input
-                        isValid = input.isNotEmpty()
-                    },
-                    placeholder = { Text(text = stringResource(id = R.string.onboarding_lastname_placeholder)) },
-                    isError = lastName.isNotEmpty(),
-                    shape = RoundedCornerShape(5.dp),
+                        .height(100.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "Little Lemon Logo",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
-                )
-            }
-            FormLabel(stringResource(id = R.string.onboarding_email))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp, 10.dp)
-            ) {
-                OutlinedTextField(
-                    value = email,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                    onValueChange = { input ->
-                        email = input
-                        isValid = input.isNotEmpty()
-                    },
-                    placeholder = { Text(text = stringResource(id = R.string.onboarding_email_placeholder)) },
-                    isError = lastName.isNotEmpty(),
-                    shape = RoundedCornerShape(5.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Row(
-                verticalAlignment = Alignment.Bottom,
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                Button(
-                    onClick =
-                    {
-                        if (isValid) {
-                            navController.navigate(Home.route)
-                        }
-                    },
-                    colors = ButtonDefaults.buttonColors(colorResource(id = R.color.primary_yellow)),
-                    shape = RoundedCornerShape(5.dp),
+                        .background(colorResource(id = R.color.primary_green))
+                        .height(120.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.onboarding_prompt),
+                        fontSize = 25.sp,
+                        lineHeight = 25.sp,
+                        textAlign = TextAlign.Center,
+                        color = colorResource(id = R.color.highlight_white),
+                        fontFamily = karlaFontFamily
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(20.dp)
+                        .height(60.dp)
                 ) {
                     Text(
-                        text = stringResource(id = R.string.onboarding_register),
+                        text = stringResource(id = R.string.onboarding_information),
                         fontSize = 20.sp,
                         lineHeight = 20.sp,
                         color = colorResource(id = R.color.highlight_black),
                         fontFamily = karlaFontFamily,
                         fontWeight = FontWeight.Bold
+                    )
+                }
+                FormLabel(stringResource(id = R.string.onboarding_firstname))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 10.dp)
+                ) {
+                    OutlinedTextField(
+                        value = firstName,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        onValueChange = { input ->
+                            firstName = input
+                            isValid = input.isNotEmpty()
+                        },
+                        placeholder = { Text(text = stringResource(id = R.string.onboarding_firstname_placeholder)) },
+                        isError = firstName.isNotEmpty(),
+                        shape = RoundedCornerShape(5.dp),
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
+                FormLabel(stringResource(id = R.string.onboarding_lastname))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 10.dp)
+                ) {
+                    OutlinedTextField(
+                        value = lastName,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        onValueChange = { input ->
+                            lastName = input
+                            isValid = input.isNotEmpty()
+                        },
+                        placeholder = { Text(text = stringResource(id = R.string.onboarding_lastname_placeholder)) },
+                        isError = lastName.isNotEmpty(),
+                        shape = RoundedCornerShape(5.dp),
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
+                FormLabel(stringResource(id = R.string.onboarding_email))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 10.dp)
+                ) {
+                    OutlinedTextField(
+                        value = email,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        onValueChange = { input ->
+                            email = input
+                            isValid = input.isNotEmpty()
+                        },
+                        placeholder = { Text(text = stringResource(id = R.string.onboarding_email_placeholder)) },
+                        isError = lastName.isNotEmpty(),
+                        shape = RoundedCornerShape(5.dp),
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
                     )
                 }
             }
