653fbb00f4f01554975b29e1d544be470afce77f
Brandon Wyie <brandon.wylie@una.ca>
2024-11-01 14:41:36 -0600

    Store remote data in local variable for menuItems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("plugin.serialization").version("2.0.0")
 }
 
 android {
@@ -68,4 +69,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 }
--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,24 +3,63 @@ package com.example.littlelemon
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
+
+private val client = HttpClient(Android) {
+    install(ContentNegotiation) {
+        json(contentType = ContentType("text", "plain"))
+    }
+}
+
+private val menuItemsLiveData = MutableLiveData<List<MenuItemNetwork>>()
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val sharedPrefs = this.getSharedPreferences(getString(R.string.prefs_title), Context.MODE_PRIVATE)
         val startDestination = sharedPrefs?.getString("destination", Onboarding.route) ?: Onboarding.route
+
+        lifecycleScope.launch {
+            val menuItems = getMenu()
+
+            runOnUiThread {
+                menuItemsLiveData.value = menuItems
+            }
+        }
+
         setContent {
             val navController = rememberNavController()
 
             Navigation(navController, startDestination)
         }
+    }
+
+    private suspend fun getMenu(): List<MenuItemNetwork> {
+        val response: MenuNetworkdata =
+            client.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+                .body()
+
+        return response.menu
     }
 }
 
